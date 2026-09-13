36c4898be92eca1525081c20417475d0f24969db
samanvaya <samanvayavats@gmail.com>
2026-09-13 21:15:55 +0530

    feat(route:project): getting all the floors by projectId

--- src/app/api/v1/project/route.ts
+++ src/app/api/v1/project/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PlotConfigurationSchemaPlotConfigurationSchemaCombined } from "@/schema/v1/project/project.schema";
-import { createProjectPlotConfiguration } from "@/services/project/project.service";
+import {
+  createProjectPlotConfiguration,
+  getAllFloorsOfTheParticularProject,
+} from "@/services/project/project.service";
 
 export async function POST(request: NextRequest) {
   try {
@@ -53,6 +56,50 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(
       {
         message: "Project configuration and registration failed",
+      },
+      { status: 500 },
+    );
+  }
+}
+
+export async function GET(request: NextRequest) {
+  try {
+    const searchParams = request.nextUrl.searchParams;
+    const projectId = searchParams.get("projectId");
+    const page = Number(searchParams.get("page")) || 1;
+    const pageSize = Number(searchParams.get("pageSize")) || 10;
+
+    if (!projectId) {
+      return NextResponse.json(
+        {
+          message: "floorId is required",
+        },
+        { status: 400 },
+      );
+    }
+
+    const project = await getAllFloorsOfTheParticularProject(projectId, page, pageSize);
+
+    if (!project) {
+      return NextResponse.json(
+        {
+          message: "Project not found",
+        },
+        { status: 500 },
+      );
+    }
+
+    return NextResponse.json({
+      project: { id: project.id, name: project.title },
+      floors: project.floors,
+      totalFloors: project._count.floors,
+    });
+  } catch (error) {
+    console.error("project not found ", error);
+
+    return NextResponse.json(
+      {
+        message: "Project not found",
       },
       { status: 500 },
     );
