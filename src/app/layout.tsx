9630a98e32b5322f7cbe68e0dc93a0fec578a853
samanvaya <samanvayavats@gmail.com>
2026-09-16 22:54:50 +0530

    feat(layout): integrate navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import { Provider } from "./provider";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import Navbar from "@/components/navbar/navbar";
+
+const inter = Inter({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-inter",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const playfair = Playfair_Display({
   subsets: ["latin"],
+  weight: ["600", "700", "800"],
+  variable: "--font-playfair",
 });
 
 export const metadata: Metadata = {
@@ -24,10 +28,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       suppressContentEditableWarning
       suppressHydrationWarning
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`h-full antialiased ${inter.variable} ${playfair.variable}`}
     >
-      <body className="min-h-full flex flex-col">
-        <Provider>{children}</Provider>
+      <body className="min-h-full flex flex-col bg-slate-950  text-white font-inter">
+        <Provider>
+          <Navbar />
+          {children}
+        </Provider>
       </body>
     </html>
   );
