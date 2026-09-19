46610bfac1891dbbc61fbb51a3863661614fd3ca
samanvaya <samanvayavats@gmail.com>
2026-09-19 23:36:20 +0530

    fix(ui): fix the main component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import React from "react";
 import { Provider } from "./provider";
 import Navbar from "@/components/navbar/navbar";
-
+import Footer from "@/components/footer/footer";
 const inter = Inter({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -30,10 +30,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       lang="en"
       className={`h-full antialiased ${inter.variable} ${playfair.variable}`}
     >
-      <body className="min-h-full flex flex-col bg-slate-950  text-white font-inter">
+      <body className="min-h-screen flex flex-col bg-slate-950 text-white font-inter">
         <Provider>
           <Navbar />
-          {children}
+          <main className="flex-1 flex flex-col justify-center items-center px-2 py-2 relative overflow-hidden">
+            {children}
+          </main>
+          <Footer />
         </Provider>
       </body>
     </html>
