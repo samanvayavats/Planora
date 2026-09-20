e0042d184868fe251efe2da139284da14b588418
samanvaya <samanvayavats@gmail.com>
2026-09-20 23:01:31 +0530

    feat(ui): heropage  component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Hero from "@/components/landing/hero";
 
 const page = () => {
   return (
     <div>
-      this is the planora
+      <Hero />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
