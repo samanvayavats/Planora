dae9e35419dc07b34f26e203b4ac793e8e9b138f
samanvaya <samanvayavats@gmail.com>
2026-09-21 22:07:19 +0530

    feat(page) :singin page added

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignInPage from "@/components/authentcation/signinpage";
+const page = () => {
+  return (
+    <div>
+      <SignInPage />
+    </div>
+  );
+};
+
+export default page;
