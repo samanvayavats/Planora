ac98c12f351142aca841e371d004ca9df6983cb1
samanvaya <samanvayavats@gmail.com>
2026-09-21 22:08:17 +0530

    feat(page) :signup page added

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignUpPage from "@/components/authentcation/signuppage";
+
+export default function Page() {
+  return (
+    <div className="flex min-h-[calc(100vh-200px)] w-full items-center justify-center p-6">
+      <SignUpPage />
+    </div>
+  );
+}
